6b18759f2b191a38d64f84fbdddcde75c505e1b8
Jukka Jylänki <jujjyl@gmail.com>
2023-09-09 22:29:29 +0300

    Update ripnmale.bat to look up bundled K4E-14 and CKPATCH.

--- ripnmake.bat
+++ ripnmake.bat
@@ -2,16 +2,20 @@
 
 cd keen4-6\static
 
+echo Deploying Keen4E 1.4 data files.
+copy /Y ..\..\K4E-14\*.CK4 .
+copy /Y ..\..\K4E-14\*.EXE .
+
 echo Trying to extract data from KEEN 4 ...
-ck4patch ripck4.pat
+..\..\CKPATCH\ck4patch ripck4.pat
 echo.
 
 echo Trying to extract data from KEEN 5 ...
-ck5patch ripck5.pat
+..\..\CKPATCH\ck5patch ripck5.pat
 echo.
 
 echo Trying to extract data from KEEN 6 ...
-ck6patch ripck6.pat
+..\..\CKPATCH\ck6patch ripck6.pat
 echo.
 
 echo Converting data files to .OBJ ...
